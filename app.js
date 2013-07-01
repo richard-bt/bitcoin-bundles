ce7a27b928af72258cd82087692f567cfc8d8578
Richard <richard@richard-acer-linux.(none)>
2013-07-01 14:30:08 -0700

    Added views and how to set up nginx with node

--- app.js
+++ app.js
@@ -8,8 +8,36 @@ var app = express();
 app.get('/', function(req, res){
  
 res.render('index.hbs');
- 
+	 
 });
+
+app.get('/success', function(req, res){
+ 
+	res.render('success.hbs');
+	 
+});
+app.get('/cancel', function(req, res){
+ 
+	res.render('cancel.hbs');
+	 
+});
+app.get('/info', function(req, res){
+ 
+	res.render('info.hbs');
+	 
+});
+app.get('/callback', function(req, res){
+	
+	var data = {};
+	data.req = req;
+	data.res = res;
+	res.render('callback.hbs', data);
+	 
+});
+
+
+
+
  
 app.listen(8080);
 
